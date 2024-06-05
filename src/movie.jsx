2a65d28f5f31cfd06ee5cd3dd42718000027fb57
Hamzat <163733983+dahpizu@users.noreply.github.com>
2024-06-05 17:17:37 +0400

    Update movie.jsx

--- src/movie.jsx
+++ src/movie.jsx
@@ -1,5 +1,30 @@
+import React from "react";
+import { CiBellOn } from "react-icons/ci";
+import { CgMenuGridO } from "react-icons/cg";
+import { MdAccountCircle } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
+
 function Movie() {
-  return <h1>Hello World</h1>;
+  return (
+    <nav className="opt">
+      <ul>
+        <li>Movies</li>
+        <li>Series</li>
+        <li>Animation</li>
+        <li>Genres</li>
+      </ul>
+      <div className="search">
+        <IoIosSearch />
+
+        <input type="text" className="search-input" />
+      </div>
+      <div className="icon">
+        <CiBellOn />
+        <CgMenuGridO />
+        <MdAccountCircle />
+      </div>
+    </nav>
+  );
 }
 
 export default Movie;
