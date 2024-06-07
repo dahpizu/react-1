3f25e774e4704c00c0c6dff877bcbc0cb5b714d0
Hamzat <163733983+dahpizu@users.noreply.github.com>
2024-06-08 03:40:38 +0400

    Update movie.jsx

--- src/movie.jsx
+++ src/movie.jsx
@@ -1,103 +1,219 @@
 import React from "react";
-import { CiBellOn } from "react-icons/ci";
-import { CgMenuGridO } from "react-icons/cg";
-import { MdAccountCircle } from "react-icons/md";
-import { IoIosSearch } from "react-icons/io";
-import { FaCodepen } from "react-icons/fa";
-import { FaHourglassStart } from "react-icons/fa6";
-import { IoStarOutline } from "react-icons/io5";
-import { IoTimeOutline } from "react-icons/io5";
+import { AiFillDribbbleCircle } from "react-icons/ai";
+import { IoMdSearch } from "react-icons/io";
+import { LuBellDot } from "react-icons/lu";
+import { CgMenuGridR } from "react-icons/cg";
+import { CgProfile } from "react-icons/cg";
+import { GoEye } from "react-icons/go";
+import { CiStar } from "react-icons/ci";
 import { CiStopwatch } from "react-icons/ci";
-import { FaRegFolder } from "react-icons/fa6";
+import { CiClock2 } from "react-icons/ci";
+import { CiFileOn } from "react-icons/ci";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
+import { PiNumberOneBold } from "react-icons/pi";
+import { PiNumberTwoBold } from "react-icons/pi";
+import { PiNumberThreeBold } from "react-icons/pi";
+import { PiNumberFourBold } from "react-icons/pi";
 
 function Movie() {
   return (
-    <div>
-      <div>
-        <nav className="opt">
-          <div className="logo-icon">
-            <FaCodepen />
+    <div className="body">
+      <div className="navbar">
+        <div className="navbar-container">
+          <div className="menu-container">
+            <ul className="menu-list">
+              <li className="menu-list-item">Movies</li>
+              <li className="menu-list-item">Series</li>
+              <li className="menu-list-item">Animation</li>
+              <li className="menu-list-item">Genre</li>
+            </ul>
           </div>
-          <ul className="option">
-            <li>Movies</li>
-            <li>Series</li>
-            <li>Animation</li>
-            <li>Genres</li>
-          </ul>
-          <div className="search">
-            <IoIosSearch />
-            search...
-            <input type="text" className="search-input" />
+          <div className="search-container">
+            <IoMdSearch />
+            <input
+              type="text"
+              name=""
+              placeholder="search"
+              className="seach-input"
+            />
           </div>
-          <div className="icon">
-            <CiBellOn />
-            <CgMenuGridO />
-            <MdAccountCircle />
+          <div className="profile-container">
+            <LuBellDot className="profile-icon" />
+            <CgMenuGridR className="profile-icon" />
+            <CgProfile className="profile-icon" />
           </div>
-        </nav>
-      </div>
-      <div className="left-nav">
-        <FaHourglassStart />
-        <IoStarOutline />
-        <CiStopwatch />
-        <IoTimeOutline />
-        <FaRegFolder />
-        <IoSettingsOutline />
-        <div className="logout">
-          <IoIosLogOut />
         </div>
       </div>
-      <div>
-        <h1>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-          deleniti sunt, alias saepe accusantium veritatis eos, illo eaque magni
-          animi asperiores, ad laboriosam accusamus ducimus totam quis dolorum
-          quas qui sapiente voluptates labore odio esse repudiandae similique!
-          Cumque quos corrupti, atque ea sed ipsum dolores labore at vel,
-          voluptatem natus repellendus quis libero, veritatis eligendi animi
-          unde ipsa. Voluptate veniam repellendus saepe porro accusantium
-          aspernatur doloribus labore iure expedita ratione, nulla sed?
-          Laboriosam ab quod beatae nobis, ipsum reiciendis maxime! Cum, illum
-          neque. Voluptate soluta eaque optio omnis ea consectetur, a voluptatum
-          nulla inventore neque non sequi modi, rerum deleniti voluptas
-          veritatis blanditiis ad animi. Cumque fuga voluptatem impedit culpa
-          omnis saepe quod quia ad perferendis vero explicabo repellendus eos
-          libero harum magnam facere, asperiores mollitia! Nemo molestiae
-          architecto maiores! Tempora, cupiditate voluptas. Odio dolorum
-          accusantium nemo excepturi, recusandae explicabo? Ullam enim
-          necessitatibus aspernatur quam hic dolor porro accusantium pariatur
-          vel quasi voluptatum expedita officia quis doloribus, alias autem
-          esse, consequatur ea nostrum maxime tempore! Rerum, fugit culpa
-          repellendus id quis similique soluta aperiam excepturi vitae commodi
-          perferendis repellat voluptatem dicta placeat, tempore laborum nihil
-          reiciendis atque dolor minima quibusdam architecto. Illum, quibusdam
-          nulla expedita exercitationem commodi libero voluptatum incidunt
-          quisquam architecto nobis facere saepe labore animi consequatur illo
-          mollitia maxime aut quia voluptatibus eum ab. Impedit tempore, cum
-          mollitia a id neque. Minima id voluptates ullam tenetur nulla quod
-          facere incidunt odit quidem consectetur aperiam molestiae harum
-          aspernatur molestias sit itaque ipsum consequatur, necessitatibus modi
-          non quia error. Deleniti rerum error minus vero recusandae mollitia
-          qui, et sunt quas minima, voluptatibus enim inventore ipsa dolore sint
-          dolorem voluptas culpa! Sapiente provident consequatur maiores, libero
-          numquam eligendi id, perferendis aliquid dolore quisquam ex vero illo
-          dolores vitae eum nam itaque deleniti cupiditate alias, sequi
-          perspiciatis nobis! Ea quam tenetur similique amet facere maxime
-          molestias aliquid laborum exercitationem non hic placeat vero unde
-          asperiores commodi, et rerum repellat voluptates libero debitis
-          praesentium nemo. Voluptates debitis quaerat, earum sequi beatae ipsam
-          provident laboriosam repellendus incidunt nam nesciunt, perferendis
-          fuga iure saepe cum blanditiis sit amet quae cumque? Dicta similique,
-          numquam laboriosam quos officiis sint voluptatum accusantium
-          doloremque, autem voluptatibus, rerum porro quis maiores praesentium
-          repellendus molestiae dolorem optio consectetur animi. Consequatur
-          ipsam hic debitis commodi, atque adipisci sed, expedita vel fugiat
-          assumenda ducimus, ullam eaque tempora et nobis quod quia illum labore
-          amet ratione nesciunt aspernatur asperiores voluptas odio. Voluptates,
-          possimus qui!
-        </h1>
+      <div className="sidebar">
+        <AiFillDribbbleCircle className="logo-icon" />
+        <GoEye className="sidebar-icon" />
+        <CiStar className="sidebar-icon" />
+        <CiStopwatch className="sidebar-icon" />
+        <CiClock2 className="sidebar-icon" />
+        <CiFileOn className="sidebar-icon" />
+        <IoSettingsOutline className="sidebar-icon" />
+        <IoIosLogOut className="sidebar-icon1" />
+      </div>
+      <div className="container">
+        <div className="movie-list-container">
+          <h1>New Release</h1>
+          <div className="movie-list-wrapper">
+            <div className="movie-list">
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/KOS.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">King Of Boys</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/beast.jpg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">
+                  Beast Of No Nation
+                </span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/titanic.avif"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Titanic</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/tribe.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">
+                  A Tribe Called Judah
+                </span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+            </div>
+            <PiNumberOneBold className="number-1" />
+            <PiNumberTwoBold className="number-2" />
+            <PiNumberThreeBold className="number-3" />
+            <PiNumberFourBold className="number-4" />
+          </div>
+        </div>
+        <div className="movie-list-container">
+          <h1>My Favorite</h1>
+          <div className="movie-list-wrapper">
+            <div className="movie-list">
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/nimbe.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Nimbe</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/omoghetti.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Omo Ghetto</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/october.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">October 1st</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/dubaibling.avif"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Dubai Bling</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+            </div>
+            <PiNumberOneBold className="number-1" />
+            <PiNumberTwoBold className="number-2" />
+            <PiNumberThreeBold className="number-3" />
+            <PiNumberFourBold className="number-4" />
+          </div>
+        </div>
+        <div className="movie-list-container">
+          <div className="movie-list-wrapper">
+            <div className="movie-list">
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/lagos.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">
+                  The Bling Lagosians
+                </span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/namaste.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Namaste Wahala</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/merrymen.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Merry Men</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+              <div className="movie-list-item">
+                <img
+                  className="movie-list-item-img"
+                  src="../public/images/aki-pawpaw.jpeg"
+                  alt=""
+                />
+                <span className="movie-list-item-title">Aki & PawPaw</span>
+                <p className="movie-list-item-rate">5.2</p>
+                <button className="movie-list-item-button">watch</button>
+              </div>
+            </div>
+            <PiNumberOneBold className="number-1" />
+            <PiNumberTwoBold className="number-2" />
+            <PiNumberThreeBold className="number-3" />
+            <PiNumberFourBold className="number-4" />
+          </div>
+        </div>
       </div>
     </div>
   );
